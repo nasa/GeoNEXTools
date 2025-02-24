5a7fc586ce16813b8012e11805393964998c768b
hhashimo-github <hirofumi.hashimoto-1@nasa.gov>
2025-02-24 15:40:16 -0800

    update data link

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,5 +1,5 @@
 # load default server names
 
-nex_server <- function() 'https://data.nas.nasa.gov/geonex/geonexdata/SUBSET/'
+nex_server <- function() 'https://data.nas.nasa.gov/gecgo/gecgodata/'
 
 
